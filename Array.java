52b1990de35501da5b1d8d1bd90981e792b246c1
Hilalibrahimaji <ihilal431@gmail.com>
2025-10-01 23:12:28 +0700

    salah tempat push

--- Array.java
+++
@@ -1,8 +0,0 @@
-public class Array {
-    public static void main(String[] args) {
-    String[] mobil = {"Toyota", "Honda", "Suzuki", "Daihatsu", "Mitsubishi"};
-    for (int i = 0; i < mobil.length; i++) {
-        System.out.println("Mobil ke-" + (i + 1) + ": " + mobil[i]);
-    }
-}
-}
